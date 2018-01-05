8d2d5b3b3ab74173e78580f18df658e3a4c2297e
dcfvg <qwsx@dcfvg.com>
2018-01-05 16:21:33 +0100

    typo in ticket template

--- site/templates/dream.php
+++ site/templates/dream.php
@@ -8,29 +8,52 @@
       <?php if($user = $site->user()): ?>
 
 
+            <h5>THE LIGHT HOUSE<br>lancelot hamelin</h5>
 
-        <h4>le rêveur</h4>
-        <p><?= $page->portraitreveur()?></p>
+            <h5>entretien n°<?= $page->dreamid()?></h5>
+            <h5><?= $page->title()?></h5>
+            <h4></h4>
 
-        <p><?= $page->nom()?>, <?= $page->sexe()?>, <?= $page->age()?> ans, <?= $page->profession()?>/</p>
+            <p>
+            <strong>date du recueil : </strong><?= $page->date('d/m/Y')?>
+            <br/><strong>heure :</strong><?= $page->heure()?>
+            <br/><strong>rapporteur du rêve : </strong><?= $page->rapporteurs()?>
+            <br/><strong>retranscription par : </strong><?= $page->retranscripteur()?>
+            <br/><strong>Lieu du receuil du récit :</strong> <?php
 
-        <p>né(e) à <?= $page->lieudenaissance()?>
-            avec pour langue maternelle le
-            <?= $page->languematernelle()?> et pour religion d'enfance <?= $page->religionenfance()?> (aujourd'hui <?= $page->religionpresente()?></p>
-        </p>
+                $pos = $page->posEntretien()->yaml();
+                if(array_key_exists('address', $pos)) echo $pos['address'];
 
-        <p> vit à <?= $page->lieudevie()?></p>
+            ?> <?= $page->lieu(); ?>
+            <br/><strong>Nom du/de la rêveu.se :</strong> <?= $page->nom()?></p>
 
-        <h4>collecteurs</h4>
-        <p><?= $page->rapporteurs()?> (rapporteur)
-        <br><?= $page->retranscripteur()?> (retranscripteur)</p>
+        <h4>portrait du/de la rêveu.se</h4>
+            <p><?= $page->portraitreveur()?></p>
 
-        <h4>situation</h4>
+            <p>
+                <?= $page->nom()?>,
+                <?php
 
-            <p><?= $page->date('d/m/Y')?>, <?= $page->heure()?>
-            <br><?=$page->posEntretien()->yaml()['address']?></p>
+                    $sexe = str_replace('féminin', 'femme', $page->sexe());
+                    $sexe = str_replace('masculin', 'homme', $sexe);
+                    echo $sexe;
 
+                ?>
+                de <?= $page->age()?> ans,
+                <?= $page->profession()?> vit à <?= $page->lieudevie()?>.
+            </p>
+
+                        <p>Né(e) à
+                <?= $page->lieudenaissance()?>
+                avec pour langue maternelle le
+                <?= $page->languematernelle()?> et dans enfance de religion <?= $page->religionenfance()?> aujourd'hui <?= $page->religionpresente()?>.</p>
+            </p>
+
+
+        <h4>impression de la rencontre</h4>
             <p><em><?= $page->situation()->kirbytext()?></em></p>
+
+
         <h4>entretien</h4>
             <p><?= $page->recit()->kirbytext()?></p>
 
