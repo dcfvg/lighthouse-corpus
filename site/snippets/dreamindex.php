0b84cac2784424ba5ad9723cfc653570c892fcfd
dcfvg <qwsx@dcfvg.com>
2018-03-11 17:16:29 +0100

    few design adjustment

--- site/snippets/dreamindex.php
+++ site/snippets/dreamindex.php
@@ -1,24 +1,34 @@
 
 <div class="content dreamIndex" id="ticket">
+
   <h5>THE LIGHT HOUSE</h5>
   <h5>index des rêves</h5>
   <svg class="barcode"
      jsbarcode-value="all"
-     jsbarcode-width="2"
-     jsbarcode-height="30"
+     jsbarcode-width="1"
+     jsbarcode-height="15"
      jsbarcode-displayValue="true"
      jsbarcode-fontSize="8"
   ></svg>
 
   <?php foreach($site->index()->filterby('intendedtemplate','dream')->visible() as $item): ?>
-    <p>
-    <svg class="barcode"
-        jsbarcode-value="<?= $item->dreamid()?>"
-       jsbarcode-width="2"
-       jsbarcode-height="30"
-       jsbarcode-displayValue="true"
-       jsbarcode-fontSize="8"
-    ></svg><br><?= $item->title() ?></p>
+
+    <div class="row">
+
+      <div class="col-xs-6">
+        <p class="codeTitle" ><?= $item->title() ?></p>
+      </div>
+
+      <div class="col-xs-6">
+        <svg class="barcode"
+            jsbarcode-value="<?= $item->dreamid()?>"
+           jsbarcode-width="1"
+           jsbarcode-height="15"
+           jsbarcode-displayValue="true"
+           jsbarcode-fontSize="6"
+        ></svg>
+      </div>
+    </div>
 
   <?php endforeach ?>
 
