c1d9e38cd9301df202652cb272281287af6094c7
dcfvg <qwsx@dcfvg.com>
2018-12-12 15:29:02 +0200

    update k

--- kirby/vendor/getkirby/toolkit/test/UrlTest.php
+++ kirby/vendor/getkirby/toolkit/test/UrlTest.php
@@ -11,6 +11,22 @@ class UrlTest extends PHPUnit_Framework_TestCase {
 
   }
 
+  public function testIsAbsolute() {
+
+    $this->assertTrue(url::isAbsolute('http://example.com/some-uri'));
+    $this->assertTrue(url::isAbsolute('https://example.com/some-uri'));
+    $this->assertTrue(url::isAbsolute('ftp://example.com/some-uri'));
+    $this->assertTrue(url::isAbsolute('some-other+123-proto.col://example.com/some-uri'));
+    $this->assertTrue(url::isAbsolute('//example.com/some-uri'));
+    $this->assertTrue(url::isAbsolute('mailto:example@example.com'));
+
+    $this->assertFalse(url::isAbsolute(''));
+    $this->assertFalse(url::isAbsolute('some-uri'));
+    $this->assertFalse(url::isAbsolute('/some-uri'));
+    $this->assertFalse(url::isAbsolute('/some-uri/?url=https://example.com'));
+
+  }
+
   public function testBuild() {
 
     $this->assertEquals('http://getkirby.com/', url::build([], 'http://getkirby.com'));
@@ -24,6 +40,28 @@ class UrlTest extends PHPUnit_Framework_TestCase {
       'scheme' => 'https',
       'host'   => 'twitter.com'
     ], 'http://getkirby.com'));
+
+    $this->assertEquals('https://getkirby.com/panel', url::build([], 'https://getkirby.com/panel'));
+    $this->assertEquals('https://getkirby.com/panel/', url::build([], 'https://getkirby.com/panel/'));
+
+    $this->assertEquals('https://getkirby.com/search/page:2?q=something', url::build([
+      'params' => ['page' => 2]
+    ], 'https://getkirby.com/search?q=something'));
+    $this->assertEquals('https://getkirby.com/search/page:2?q=something', url::build([
+      'params' => ['page' => 2]
+    ], 'https://getkirby.com/search/?q=something'));
+    $this->assertEquals('https://getkirby.com/search/page:3?q=something', url::build([
+      'params' => ['page' => 3]
+    ], 'https://getkirby.com/search/page:2?q=something'));
+    $this->assertEquals('https://getkirby.com/search/?q=something', url::build([
+      'params' => []
+    ], 'https://getkirby.com/search/?q=something'));
+    $this->assertEquals('https://getkirby.com/search?q=something', url::build([
+      'params' => []
+    ], 'https://getkirby.com/search?q=something'));
+    $this->assertEquals('https://getkirby.com/search?q=something', url::build([
+      'params' => []
+    ], 'https://getkirby.com/search/page:2?q=something'));
 
     $this->assertEquals('https://getkirby.com/some/url/with:params?and=query#hash', url::build([], 'https://getkirby.com/some/url/with:params?and=query#hash'));
     $this->assertEquals('https://getkirby.com/some/otherurl/with:otherparams/and:moreparams?anddifferent=query#differenthash', url::build([
