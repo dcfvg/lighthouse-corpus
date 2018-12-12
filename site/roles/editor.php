20c8b42170e287b19616f9e3742943dff3a28233
dcfvg <qwsx@dcfvg.com>
2018-12-12 15:26:30 +0200

    setup editor role

---
+++ site/roles/editor.php
@@ -0,0 +1,31 @@
+<?php
+return [
+  'name'        => 'Editor',
+  'default'     => false,
+  'permissions' => [
+    'panel.page.read' => function() {
+      return $this->target()->page()->template() === 'dream';
+    },
+    'panel.site.update' => false,
+    'panel.access.options' => false,
+    'panel.widget.pages' => false,
+    'panel.page.create' => function() {
+      return $this->target()->page()->template() === 'dreams';
+    },
+    'panel.page.update' => function() {
+      return $this->target()->page()->template() === 'dream';
+    },
+    'panel.page.delete' => false,
+    'panel.page.visibility' => false,
+    'panel.page.url' => false,
+    'panel.file.sort' => false,
+
+    'panel.user.*' => function() {
+      return $this->user()->is($this->target()->user());
+    },
+    'panel.user.create' => false,
+    'panel.user.delete' => false
+  ]
+];
+
+?>
